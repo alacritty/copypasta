c977d95d439bad48045b4c5aa842afc3ec6c5dd2
Christian Duerr <contact@christianduerr.com>
2020-03-07 22:17:34 +0100

    Remove unnecessary return statement

--- src/osx_clipboard.rs
+++ src/osx_clipboard.rs
@@ -70,11 +70,11 @@ impl ClipboardProvider for OSXClipboardContext {
         let string_array = NSArray::from_vec(vec![NSString::from_str(&data)]);
         let _: usize = unsafe { msg_send![self.pasteboard, clearContents] };
         let success: bool = unsafe { msg_send![self.pasteboard, writeObjects: string_array] };
-        return if success {
+        if success {
             Ok(())
         } else {
             Err("NSPasteboard#writeObjects: returned false".into())
-        };
+        }
     }
 }
 
