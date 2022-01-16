ecafec9b6f944c38c99c4fc6cab4d9c65d21ecb6
Kirill Chibisov <contact@kchibisov.com>
2022-01-16 09:33:18 +0300

    Migrate to 2021 edition

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#![deny(clippy::all, clippy::if_not_else, clippy::enum_glob_use, clippy::wrong_pub_self_convention)]
+#![deny(clippy::all, clippy::if_not_else, clippy::enum_glob_use)]
 
 mod common;
 pub use crate::common::ClipboardProvider;
