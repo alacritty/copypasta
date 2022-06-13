fd7f0958b0b2137e0045badff49f1e97aeb33792
xliiv <xliiv@protonmail.com>
2022-06-13 16:16:04 +0200

    Add println to hello_world example
    Co-authored-by: Christian Duerr <contact@christianduerr.com>

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -3,7 +3,10 @@ use copypasta::{ClipboardContext, ClipboardProvider};
 fn main() {
     let mut ctx = ClipboardContext::new().unwrap();
 
-    let the_string = "Hello, world!";
+    let msg = "Hello, world!";
+    ctx.set_contents(msg.to_owned()).unwrap();
 
-    ctx.set_contents(the_string.to_owned()).unwrap();
+    let content = ctx.get_contents().unwrap();
+
+    println!("{}", content);
 }
